cb9689a0100ee39e08117cdc86188876830eebae
KhaledHAnter <khaldntr11@gmail.com>
2025-05-17 13:21:53 +0300

    - add localization ("ar","en"). - add easy localization package. - add the code that toggles the lang. - save the lang locally & asign it bt the system lan at the first run. - fix the theme toggle problem

--- lib/features/home/ui/views/home_screen.dart
+++ lib/features/home/ui/views/home_screen.dart
@@ -1,10 +1,27 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:saay/core/helpers/app_strings.dart';
+import 'package:saay/core/helpers/constants.dart';
+import 'package:saay/core/helpers/shared_prefernce_helper.dart';
 import 'package:saay/core/theming/styles.dart';
 import 'package:saay/core/theming/theming_cubit/theme_cubit.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  Future<void> toggleLanguage(BuildContext context) async {
+    final currentLang = context.locale.languageCode;
+
+    // حدد اللغة الجديدة
+    final newLang = currentLang == 'ar' ? 'en' : 'ar';
+
+    // احفظ اللغة الجديدة محليًا
+    await SharedPrefHelper.setData(langCacheKey, newLang);
+
+    // غيّر اللغة باستخدام EasyLocalization
+    await context.setLocale(Locale(newLang));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +33,19 @@ class HomeScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text(
-                "Home View",
+                AppStrings.homeScreenTitle.tr(),
                 style: Styles.font16SemiBold(context),
               ), // مثال زر لتغيير الثيم (في أي شاشة):
+              Gap(20),
               ElevatedButton(
-                onPressed: () => context.read<ThemeCubit>().toggleTheme(),
-                child: Text('تبديل الثيم'),
+                onPressed:
+                    () => context.read<ThemeCubit>().toggleTheme(context),
+                child: Text(AppStrings.changeTheme.tr()),
+              ),
+              Gap(20),
+              ElevatedButton(
+                onPressed: () => toggleLanguage(context),
+                child: Text(AppStrings.changeLanguage.tr()),
               ),
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:saay/core/di/dependency_injection.dart';
+import 'package:saay/core/helpers/constants.dart';
+import 'package:saay/core/helpers/shared_prefernce_helper.dart';
 import 'package:saay/core/routing/app_router.dart';
 import 'package:saay/saay.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await setupGetIt();
-  runApp(Saay(appRouter: AppRouter()));
+  String? langFromStorage = await SharedPrefHelper.getString(langCacheKey);
+
+  if (langFromStorage == null || langFromStorage.isEmpty) {
+    final String defaultLang = PlatformDispatcher.instance.locale.languageCode;
+    final supported = ['en', 'ar'];
+    final fallbackLang = supported.contains(defaultLang) ? defaultLang : 'ar';
+
+    await SharedPrefHelper.setData(langCacheKey, fallbackLang);
+    langFromStorage = fallbackLang;
+  }
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale("en"), Locale("ar")],
+      path: langPath,
+      fallbackLocale: const Locale("en"),
+      startLocale: Locale(langFromStorage),
+      child: Saay(appRouter: AppRouter()),
+    ),
+  );
 }
